e8debdfe021cf516cc10fb890dd437753ac6cf03
kwonssshyeon <kwonsh4396@gmail.com>
2024-04-01 17:59:16 +0900

    Fix: modified USER_AGENT, ROBOTSTXT_OBEY
    avoid block from robots.txt

--- knowckknowck_crawling/knowckknowck_crawling/settings.py
+++ knowckknowck_crawling/knowckknowck_crawling/settings.py
@@ -15,9 +15,10 @@ NEWSPIDER_MODULE = "knowckknowck_crawling.spiders"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "knowckknowck_crawling (+http://www.yourdomain.com)"
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:39.0) Gecko/20100101 Firefox/39.0'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
