ee0d06b718868dc1e00f7ad692a8a5b625dff2ef
kwonssshyeon <kwonsh4396@gmail.com>
2024-04-03 15:21:18 +0900

    Fix: 기사 본문 불필요한 이미지 정보 등 제거
    BeautifulSoup 이용

--- knowckknowck_crawling/knowckknowck_crawling/spiders/url_crawler.py
+++ knowckknowck_crawling/knowckknowck_crawling/spiders/url_crawler.py
@@ -47,6 +47,14 @@ class CrawlerSpider(CrawlSpider):
         item['category']=categories["10"+response._get_url()[-1]]
         item['title']=response.xpath('//div[@class="media_end_head_title"]/h2/span/text()').get()
         item['created_at']=response.xpath('//div[@class="media_end_head_info_datestamp"]//span/@data-date-time').get()
-        item['content']=response.xpath('//article[@class="go_trans _article_content"]').get()
+        
+        pre_content = response.xpath('//article[@class="go_trans _article_content"]').get()
+        content = bs(pre_content, 'html.parser')
+        content = content.select_one('#dic_area')
+
+        for div in content.find_all('div'):
+            div.clear()
+        item['content']=content.get_text()
+
 
         yield item
