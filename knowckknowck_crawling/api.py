4fd8eb29e7c04c5e908d019e3d9d59a974d0d358
kwonssshyeon <kwonsh4396@gmail.com>
2024-04-22 21:33:43 +0900

    Refac: fastapi 관련 함수 및 모듈 삭제 & 가상환경 python 3.10 으로 변경

--- knowckknowck_crawling/api.py
+++ knowckknowck_crawling/api.py
@@ -1,11 +1,9 @@
-from fastapi import FastAPI
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 import time
 
 
 def api():
-    
     process = CrawlerProcess(get_project_settings())
     process.crawl('url_crawler')
     process.start()
