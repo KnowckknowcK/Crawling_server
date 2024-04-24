ddcbe63c5b9f070b1051ac3ec088abde306a2245
kwonssshyeon <kwonsh4396@gmail.com>
2024-04-24 23:29:43 +0900

    Feat: RSS 크롤러 실행 함수 작성

---
+++ knowckknowck_crawling/rss_cralwer.py
@@ -0,0 +1,12 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+import time
+
+
+def excute():
+    process = CrawlerProcess(get_project_settings())
+    process.crawl('rss_crawler')
+    process.start()
+
+if __name__=='__main__':
+    excute()
