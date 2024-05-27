0c5338137857d6b49de8d9092ff2f2be724673b1
kwonssshyeon <kwonsh4396@gmail.com>
2024-05-27 15:55:51 +0900

    Feat: id auto increase -> 직접 설정하도록 쿼리 및 모델 변경

--- knowckknowck_crawling/knowckknowck_crawling/pipelines.py
+++ knowckknowck_crawling/knowckknowck_crawling/pipelines.py
@@ -31,7 +31,8 @@ class KnowckknowckDataPipeline:
         self.cur = self.conn.cursor()
 
     def process_item(self,item,spider):
-        self.cur.execute(""" insert into article (created_time,article_url,category,title,content) values (%s,%s,%s,%s,%s)""", (
+        self.cur.execute(""" insert into article (id,created_time,article_url,category,title,content) values (%s,%s,%s,%s,%s,%s)""", (
+            item["id"],
             item["created_at"],
             item["original_url"],
             item["category"],
